a2e8b2c7f58dc990af30451a735959d009fc1376
prerit13 <147128014+prerit13@users.noreply.github.com>
2025-06-08 19:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateTimer() {
+      const now = new Date();
+      const formattedTime = now.toLocaleString(); // e.g., "6/8/2025, 7:21:35 PM"
+      document.getElementById('timer').textContent = formattedTime;
+    }
+
+    // Initial call
+    updateTimer();
+
+    // Update every second
+    setInterval(updateTimer, 1000)
